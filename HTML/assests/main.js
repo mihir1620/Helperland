045f83ef4e66135c8ba1fa3e8fe851be1f851151
Mihir Rathod <mihirrathod1612@gmail.com>
2022-01-12 18:29:08 +0530

    making book-service page

--- HTML/assests/main.js
+++ HTML/assests/main.js
@@ -14,7 +14,42 @@ $(window).scroll(function(){
 });
 
 
-
 $(document).ready(function() {
 $('#example').DataTable();
 } );
+
+function show1(){
+
+  document.getElementById('div1').style="display:flex;";
+  document.getElementById('div2').style.display="none";
+  document.getElementById('div3').style.display="none";
+  document.getElementById('div4').style.display="none";
+}
+
+function show2(){
+  document.getElementById('div1').style.display="none";
+  document.getElementById('div2').style.display="block";
+  document.getElementById('div3').style.display="none";
+  document.getElementById('div4').style.display="none";
+}
+
+function show3(){
+  document.getElementById('div1').style.display="none";
+  document.getElementById('div2').style.display="none";
+  document.getElementById('div3').style.display="flex";
+  document.getElementById('div4').style.display="none";
+}
+
+function show4(){
+  document.getElementById('div1').style.display="none";
+  document.getElementById('div2').style.display="none";
+  document.getElementById('div3').style.display="none";
+  document.getElementById('div4').style.display="flex";
+}
+
+$('#datepicker').datepicker({  
+});  
+
+// function services(){
+//   document.getElementById('ser_1')
+// }
