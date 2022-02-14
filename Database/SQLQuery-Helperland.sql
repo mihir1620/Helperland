1801a5badec1691a60cd8c6b806f47cd74a7addd
Mihir Rathod <mihirrathod1612@gmail.com>
2022-02-14 17:38:05 +0530

    SQL schema

--- Database/SQLQuery-Helperland.sql
+++ Database/SQLQuery-Helperland.sql
@@ -1,107 +1,428 @@
-Create database Helperland
-
-Create Table tblUser
+USE [master]
+GO
+/****** Object:  Database [Helperland]    Script Date: 14-02-2022 17:34:57 ******/
+CREATE DATABASE [Helperland]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'Helperland', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\Helperland.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'Helperland_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\Helperland_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT
+GO
+ALTER DATABASE [Helperland] SET COMPATIBILITY_LEVEL = 150
+GO
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [Helperland].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+ALTER DATABASE [Helperland] SET ANSI_NULL_DEFAULT OFF 
+GO
+ALTER DATABASE [Helperland] SET ANSI_NULLS OFF 
+GO
+ALTER DATABASE [Helperland] SET ANSI_PADDING OFF 
+GO
+ALTER DATABASE [Helperland] SET ANSI_WARNINGS OFF 
+GO
+ALTER DATABASE [Helperland] SET ARITHABORT OFF 
+GO
+ALTER DATABASE [Helperland] SET AUTO_CLOSE OFF 
+GO
+ALTER DATABASE [Helperland] SET AUTO_SHRINK OFF 
+GO
+ALTER DATABASE [Helperland] SET AUTO_UPDATE_STATISTICS ON 
+GO
+ALTER DATABASE [Helperland] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+ALTER DATABASE [Helperland] SET CURSOR_DEFAULT  GLOBAL 
+GO
+ALTER DATABASE [Helperland] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+ALTER DATABASE [Helperland] SET NUMERIC_ROUNDABORT OFF 
+GO
+ALTER DATABASE [Helperland] SET QUOTED_IDENTIFIER OFF 
+GO
+ALTER DATABASE [Helperland] SET RECURSIVE_TRIGGERS OFF 
+GO
+ALTER DATABASE [Helperland] SET  DISABLE_BROKER 
+GO
+ALTER DATABASE [Helperland] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+ALTER DATABASE [Helperland] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+ALTER DATABASE [Helperland] SET TRUSTWORTHY OFF 
+GO
+ALTER DATABASE [Helperland] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+ALTER DATABASE [Helperland] SET PARAMETERIZATION SIMPLE 
+GO
+ALTER DATABASE [Helperland] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+ALTER DATABASE [Helperland] SET HONOR_BROKER_PRIORITY OFF 
+GO
+ALTER DATABASE [Helperland] SET RECOVERY FULL 
+GO
+ALTER DATABASE [Helperland] SET  MULTI_USER 
+GO
+ALTER DATABASE [Helperland] SET PAGE_VERIFY CHECKSUM  
+GO
+ALTER DATABASE [Helperland] SET DB_CHAINING OFF 
+GO
+ALTER DATABASE [Helperland] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+ALTER DATABASE [Helperland] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+ALTER DATABASE [Helperland] SET DELAYED_DURABILITY = DISABLED 
+GO
+ALTER DATABASE [Helperland] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+EXEC sys.sp_db_vardecimal_storage_format N'Helperland', N'ON'
+GO
+ALTER DATABASE [Helperland] SET QUERY_STORE = OFF
+GO
+USE [Helperland]
+GO
+/****** Object:  Table [dbo].[City]    Script Date: 14-02-2022 17:34:58 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[City](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[CityName] [nvarchar](50) NOT NULL,
+	[StateId] [int] NOT NULL,
+ CONSTRAINT [PK_City] PRIMARY KEY CLUSTERED 
 (
-	user_id int Primary Key NOT NULL,
-	user_first_name varchar(50) NOT NULL,
-	user_last_name varchar(50) NOT NULL,
-	user_email_id varchar(50) NOT NULL,
-	user_mobile_no int NOT NULL,
-	user_DOB date NOT NULL,
-	user_address varchar(100) NOT NULL,
-	user_pincode int NOT NULL,
-	preferred_lang varchar(50),
-	user_old_pswd varchar(50) NOT NULL,
-	user_new_pswd varchar(50) NOT NULL
-)
-
-Create Table tblService_Provider
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[ContactUs]    Script Date: 14-02-2022 17:34:58 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ContactUs](
+	[ContactUsId] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NOT NULL,
+	[Email] [nvarchar](200) NOT NULL,
+	[Subject] [nvarchar](500) NULL,
+	[PhoneNumber] [nvarchar](20) NOT NULL,
+	[Message] [nvarchar](max) NOT NULL,
+	[UploadFileName] [nvarchar](100) NULL,
+	[CreatedOn] [datetime] NULL,
+	[CreatedBy] [int] NULL,
+	[FileName] [varchar](500) NULL,
+ CONSTRAINT [PK_ContactUs] PRIMARY KEY CLUSTERED 
 (
-	provider_id int Primary Key NOT NULL,
-	provider_first_name varchar(50) NOT NULL,
-	provider_last_name varchar(50) NOT NULL,
-	provider_email_id varchar(50) NOT NULL,
-	provider_mobile_no int NOT NULL,
-	provider_DOB date NOT NULL,
-	provider_nationality varchar(30) NOT NULL,
-	provider_gender varchar(10) NOT NULL,
-	provider_pincode int NOT NULL,
-	provider_house_no int NOT NULL,
-	provider_street_no int NOT NULL,
-	provider_city varchar(50) NOT NULL,
-	provider_old_pswd varchar(50) NOT NULL,
-	provider_new_pswd varchar(50) NOT NULL
-)
-
-Create Table tblAll_Service
+	[ContactUsId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[FavoriteAndBlocked]    Script Date: 14-02-2022 17:34:58 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[FavoriteAndBlocked](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[UserId] [int] NOT NULL,
+	[TargetUserId] [int] NOT NULL,
+	[IsFavorite] [bit] NOT NULL,
+	[IsBlocked] [bit] NOT NULL,
+ CONSTRAINT [PK_FavoriteAndBlocked] PRIMARY KEY CLUSTERED 
 (
-	service_id int Primary Key NOT NULL,
-	provider_id int  NOT NULL,
-	user_id int NOT NULL,
-	service_status varchar(50) NOT NULL,
-	service_pincode int NOT NULL,
-	service_date date NOT NULL,
-	rescheduled_service_date date,
-	service_time time NOT NULL,
-	rescheduled_service_time time,
-	totalservice__hours  int NOT NULL,
-	extra_services  varchar(50),
-	service_payment float NOT NULL,
-	service_comments varchar(50),
-	have_pat bit NOT NULL
-)
-
-Alter table tblAll_Service 
-add constraint user_id_FK FOREIGN KEY (user_id) references tblUser(user_id)
-
-Alter table tblAll_Service 
-add constraint provider_id_FK FOREIGN KEY (provider_id) references tblService_Provider(provider_id)
-
-Create Table tblOngoing_Service
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Rating]    Script Date: 14-02-2022 17:34:58 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Rating](
+	[RatingId] [int] IDENTITY(1,1) NOT NULL,
+	[ServiceRequestId] [int] NOT NULL,
+	[RatingFrom] [int] NOT NULL,
+	[RatingTo] [int] NOT NULL,
+	[Ratings] [decimal](2, 1) NOT NULL,
+	[Comments] [nvarchar](2000) NULL,
+	[RatingDate] [datetime] NOT NULL,
+	[OnTimeArrival] [decimal](2, 1) NOT NULL,
+	[Friendly] [decimal](2, 1) NOT NULL,
+	[QualityOfService] [decimal](2, 1) NOT NULL,
+ CONSTRAINT [PK_Rating] PRIMARY KEY CLUSTERED 
 (
-	ongoing_service_id int NOT NULL,
-	ongoing_provider_id int  NOT NULL,
-	ongoing_service_date date NOT NULL,
-	ongoing_rescheduled_service_date date,
-	ongoing_service_time time NOT NULL,
-	ongoing_rescheduled_service_time time,
-	ongoing_service_accepted bit NOT NULL
-)
-
-Alter table tblOngoing_Service 
-add constraint service_id_FK FOREIGN KEY (ongoing_service_id) references tblAll_Service(service_id)
-
-Alter table tblOngoing_Service 
-add constraint ongoing_provider_id_FK FOREIGN KEY (ongoing_provider_id) references tblService_Provider(provider_id)
-
-Create Table tblNew_Service
+	[RatingId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[ServiceRequest]    Script Date: 14-02-2022 17:34:58 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ServiceRequest](
+	[ServiceRequestId] [int] IDENTITY(1,1) NOT NULL,
+	[UserId] [int] NOT NULL,
+	[ServiceId] [int] NOT NULL,
+	[ServiceStartDate] [datetime] NOT NULL,
+	[ZipCode] [nvarchar](10) NOT NULL,
+	[ServiceHourlyRate] [decimal](8, 2) NULL,
+	[ServiceHours] [float] NOT NULL,
+	[ExtraHours] [float] NULL,
+	[SubTotal] [decimal](8, 2) NOT NULL,
+	[Discount] [decimal](8, 2) NULL,
+	[TotalCost] [decimal](8, 2) NOT NULL,
+	[Comments] [nvarchar](500) NULL,
+	[PaymentTransactionRefNo] [nvarchar](50) NULL,
+	[PaymentDue] [bit] NOT NULL,
+	[ServiceProviderId] [int] NULL,
+	[SPAcceptedDate] [datetime] NULL,
+	[HasPets] [bit] NOT NULL,
+	[Status] [int] NULL,
+	[CreatedDate] [datetime] NOT NULL,
+	[ModifiedDate] [datetime] NOT NULL,
+	[ModifiedBy] [int] NULL,
+	[RefundedAmount] [decimal](8, 2) NULL,
+	[Distance] [decimal](18, 2) NOT NULL,
+	[HasIssue] [bit] NULL,
+	[PaymentDone] [bit] NULL,
+	[RecordVersion] [uniqueidentifier] NULL,
+ CONSTRAINT [PK_ServiceRequest] PRIMARY KEY CLUSTERED 
 (
-	new_service_id int  NOT NULL,
-	new_provider_id int  NOT NULL,
-	service_status varchar(50) NOT NULL
-)
-
-Alter table tblNew_Service 
-add constraint new_service_id_FK FOREIGN KEY (new_service_id) references tblAll_Service(service_id)
-
-Alter table tblNew_Service 
-add constraint new_provider_id_FK FOREIGN KEY (new_provider_id) references tblService_Provider(provider_id)
-
-Create Table tblContact_form
+	[ServiceRequestId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[ServiceRequestAddress]    Script Date: 14-02-2022 17:34:58 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ServiceRequestAddress](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[ServiceRequestId] [int] NULL,
+	[AddressLine1] [nvarchar](200) NULL,
+	[AddressLine2] [nvarchar](200) NULL,
+	[City] [nvarchar](50) NULL,
+	[State] [nvarchar](50) NULL,
+	[PostalCode] [nvarchar](20) NULL,
+	[Mobile] [nvarchar](20) NULL,
+	[Email] [nvarchar](100) NULL,
+ CONSTRAINT [PK_ServiceRequestAddress] PRIMARY KEY CLUSTERED 
 (
-	contact_id int Primary Key NOT NULL,
-	contact_first_name  varchar(50) NOT NULL,
-	contact_last_name varchar(50) NOT NULL,
-	contact_mobile_no int NOT NULL,
-	contact_purpose varchar(50) NOT NULL,
-	contact_message varchar(50) NOT NULL,
-)
-
-Create Table tblSaved_cards
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[ServiceRequestExtra]    Script Date: 14-02-2022 17:34:58 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ServiceRequestExtra](
+	[ServiceRequestExtraId] [int] IDENTITY(1,1) NOT NULL,
+	[ServiceRequestId] [int] NOT NULL,
+	[ServiceExtraId] [int] NOT NULL,
+ CONSTRAINT [PK_ServiceRequestExtra] PRIMARY KEY CLUSTERED 
 (
-	card_number int Primary Key NOT NULL,
-	user_id int NOT NULL,
-)
-
-Alter Table tblSaved_cards
-add constraint user_card_id_FK FOREIGN KEY (user_id) references tblUser(user_id)
+	[ServiceRequestExtraId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[State]    Script Date: 14-02-2022 17:34:58 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[State](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[StateName] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_State] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Test]    Script Date: 14-02-2022 17:34:58 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Test](
+	[TestId] [int] NOT NULL,
+	[TestName] [varchar](50) NULL
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[User]    Script Date: 14-02-2022 17:34:58 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[User](
+	[UserId] [int] IDENTITY(1,1) NOT NULL,
+	[FirstName] [nvarchar](100) NOT NULL,
+	[LastName] [nvarchar](100) NOT NULL,
+	[Email] [nvarchar](100) NOT NULL,
+	[Password] [nvarchar](100) NULL,
+	[Mobile] [nvarchar](20) NOT NULL,
+	[UserTypeId] [int] NOT NULL,
+	[Gender] [int] NULL,
+	[DateOfBirth] [datetime] NULL,
+	[UserProfilePicture] [nvarchar](200) NULL,
+	[IsRegisteredUser] [bit] NOT NULL,
+	[PaymentGatewayUserRef] [nvarchar](200) NULL,
+	[ZipCode] [nvarchar](20) NULL,
+	[WorksWithPets] [bit] NOT NULL,
+	[LanguageId] [int] NULL,
+	[NationalityId] [int] NULL,
+	[CreatedDate] [datetime] NOT NULL,
+	[ModifiedDate] [datetime] NOT NULL,
+	[ModifiedBy] [int] NOT NULL,
+	[IsApproved] [bit] NOT NULL,
+	[IsActive] [bit] NOT NULL,
+	[IsDeleted] [bit] NOT NULL,
+	[Status] [int] NULL,
+	[BankTokenId] [nvarchar](100) NULL,
+	[TaxNo] [nvarchar](50) NULL,
+	[ReserPasswordLink] [nchar](100) NULL,
+ CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED 
+(
+	[UserId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[UserAddress]    Script Date: 14-02-2022 17:34:58 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[UserAddress](
+	[AddressId] [int] IDENTITY(1,1) NOT NULL,
+	[UserId] [int] NOT NULL,
+	[AddressLine1] [nvarchar](200) NOT NULL,
+	[AddressLine2] [nvarchar](200) NULL,
+	[City] [nvarchar](50) NOT NULL,
+	[State] [nvarchar](50) NULL,
+	[PostalCode] [nvarchar](20) NOT NULL,
+	[IsDefault] [bit] NOT NULL,
+	[IsDeleted] [bit] NOT NULL,
+	[Mobile] [nvarchar](20) NULL,
+	[Email] [nvarchar](100) NULL,
+ CONSTRAINT [PK_UserAddresses] PRIMARY KEY CLUSTERED 
+(
+	[AddressId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Zipcode]    Script Date: 14-02-2022 17:34:58 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Zipcode](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[ZipcodeValue] [nvarchar](50) NOT NULL,
+	[CityId] [int] NOT NULL,
+ CONSTRAINT [PK_Zipcode] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Rating] ADD  CONSTRAINT [DF_Rating_OnTimeArrival]  DEFAULT ((0)) FOR [OnTimeArrival]
+GO
+ALTER TABLE [dbo].[Rating] ADD  CONSTRAINT [DF_Rating_Friendly]  DEFAULT ((0)) FOR [Friendly]
+GO
+ALTER TABLE [dbo].[Rating] ADD  CONSTRAINT [DF_Rating_QualityOfService]  DEFAULT ((0)) FOR [QualityOfService]
+GO
+ALTER TABLE [dbo].[ServiceRequest] ADD  CONSTRAINT [DF_ServiceRequest_PaymentDue]  DEFAULT ((0)) FOR [PaymentDue]
+GO
+ALTER TABLE [dbo].[ServiceRequest] ADD  CONSTRAINT [DF_ServiceRequest_IsPet]  DEFAULT ((0)) FOR [HasPets]
+GO
+ALTER TABLE [dbo].[ServiceRequest] ADD  CONSTRAINT [DF_ServiceRequest_CreatedDate]  DEFAULT (getdate()) FOR [CreatedDate]
+GO
+ALTER TABLE [dbo].[ServiceRequest] ADD  CONSTRAINT [DF_ServiceRequest_ModifiedDate]  DEFAULT (getdate()) FOR [ModifiedDate]
+GO
+ALTER TABLE [dbo].[ServiceRequest] ADD  CONSTRAINT [DF_ServiceRequest_Distance]  DEFAULT ((0)) FOR [Distance]
+GO
+ALTER TABLE [dbo].[User] ADD  CONSTRAINT [DF_User_IsRegisteredUser]  DEFAULT ((0)) FOR [IsRegisteredUser]
+GO
+ALTER TABLE [dbo].[User] ADD  CONSTRAINT [DF_User_WorksWithPets]  DEFAULT ((0)) FOR [WorksWithPets]
+GO
+ALTER TABLE [dbo].[User] ADD  CONSTRAINT [DF_User_IsApproved]  DEFAULT ((0)) FOR [IsApproved]
+GO
+ALTER TABLE [dbo].[User] ADD  CONSTRAINT [DF_User_IsActive]  DEFAULT ((0)) FOR [IsActive]
+GO
+ALTER TABLE [dbo].[User] ADD  CONSTRAINT [DF_User_IsDeleted]  DEFAULT ((0)) FOR [IsDeleted]
+GO
+ALTER TABLE [dbo].[UserAddress] ADD  CONSTRAINT [DF_UserAddresses_IsDefault]  DEFAULT ((0)) FOR [IsDefault]
+GO
+ALTER TABLE [dbo].[UserAddress] ADD  CONSTRAINT [DF_UserAddresses_IsDeleted]  DEFAULT ((0)) FOR [IsDeleted]
+GO
+ALTER TABLE [dbo].[City]  WITH CHECK ADD  CONSTRAINT [FK_City_State] FOREIGN KEY([StateId])
+REFERENCES [dbo].[State] ([Id])
+GO
+ALTER TABLE [dbo].[City] CHECK CONSTRAINT [FK_City_State]
+GO
+ALTER TABLE [dbo].[FavoriteAndBlocked]  WITH CHECK ADD  CONSTRAINT [FK_FavoriteAndBlocked_FavoriteAndBlocked] FOREIGN KEY([UserId])
+REFERENCES [dbo].[User] ([UserId])
+GO
+ALTER TABLE [dbo].[FavoriteAndBlocked] CHECK CONSTRAINT [FK_FavoriteAndBlocked_FavoriteAndBlocked]
+GO
+ALTER TABLE [dbo].[FavoriteAndBlocked]  WITH CHECK ADD  CONSTRAINT [FK_FavoriteAndBlocked_User] FOREIGN KEY([TargetUserId])
+REFERENCES [dbo].[User] ([UserId])
+GO
+ALTER TABLE [dbo].[FavoriteAndBlocked] CHECK CONSTRAINT [FK_FavoriteAndBlocked_User]
+GO
+ALTER TABLE [dbo].[Rating]  WITH CHECK ADD  CONSTRAINT [FK_Rating_ServiceRequest] FOREIGN KEY([ServiceRequestId])
+REFERENCES [dbo].[ServiceRequest] ([ServiceRequestId])
+GO
+ALTER TABLE [dbo].[Rating] CHECK CONSTRAINT [FK_Rating_ServiceRequest]
+GO
+ALTER TABLE [dbo].[Rating]  WITH CHECK ADD  CONSTRAINT [FK_Rating_User] FOREIGN KEY([RatingFrom])
+REFERENCES [dbo].[User] ([UserId])
+GO
+ALTER TABLE [dbo].[Rating] CHECK CONSTRAINT [FK_Rating_User]
+GO
+ALTER TABLE [dbo].[Rating]  WITH CHECK ADD  CONSTRAINT [FK_Rating_User1] FOREIGN KEY([RatingTo])
+REFERENCES [dbo].[User] ([UserId])
+GO
+ALTER TABLE [dbo].[Rating] CHECK CONSTRAINT [FK_Rating_User1]
+GO
+ALTER TABLE [dbo].[ServiceRequest]  WITH CHECK ADD  CONSTRAINT [FK_ServiceRequest_User] FOREIGN KEY([UserId])
+REFERENCES [dbo].[User] ([UserId])
+GO
+ALTER TABLE [dbo].[ServiceRequest] CHECK CONSTRAINT [FK_ServiceRequest_User]
+GO
+ALTER TABLE [dbo].[ServiceRequest]  WITH CHECK ADD  CONSTRAINT [FK_ServiceRequest_User1] FOREIGN KEY([ServiceProviderId])
+REFERENCES [dbo].[User] ([UserId])
+GO
+ALTER TABLE [dbo].[ServiceRequest] CHECK CONSTRAINT [FK_ServiceRequest_User1]
+GO
+ALTER TABLE [dbo].[ServiceRequestAddress]  WITH CHECK ADD  CONSTRAINT [FK_ServiceRequestAddress_ServiceRequest] FOREIGN KEY([ServiceRequestId])
+REFERENCES [dbo].[ServiceRequest] ([ServiceRequestId])
+GO
+ALTER TABLE [dbo].[ServiceRequestAddress] CHECK CONSTRAINT [FK_ServiceRequestAddress_ServiceRequest]
+GO
+ALTER TABLE [dbo].[ServiceRequestExtra]  WITH CHECK ADD  CONSTRAINT [FK_ServiceRequestExtra_ServiceRequest] FOREIGN KEY([ServiceRequestId])
+REFERENCES [dbo].[ServiceRequest] ([ServiceRequestId])
+GO
+ALTER TABLE [dbo].[ServiceRequestExtra] CHECK CONSTRAINT [FK_ServiceRequestExtra_ServiceRequest]
+GO
+ALTER TABLE [dbo].[UserAddress]  WITH CHECK ADD  CONSTRAINT [FK_UserAddresses_User] FOREIGN KEY([UserId])
+REFERENCES [dbo].[User] ([UserId])
+GO
+ALTER TABLE [dbo].[UserAddress] CHECK CONSTRAINT [FK_UserAddresses_User]
+GO
+ALTER TABLE [dbo].[Zipcode]  WITH CHECK ADD  CONSTRAINT [FK_Zipcode_City] FOREIGN KEY([CityId])
+REFERENCES [dbo].[City] ([Id])
+GO
+ALTER TABLE [dbo].[Zipcode] CHECK CONSTRAINT [FK_Zipcode_City]
+GO
+USE [master]
+GO
+ALTER DATABASE [Helperland] SET  READ_WRITE 
+GO
